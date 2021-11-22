f2ba5c61ccb1dd8924fd1a828325741343bb32eb
Constancegit status <constanceingram94@gmail.com>
2021-11-21 19:12:49 -0600

    fixing mobile responsive sizing

--- components/portfolio.js
+++ components/portfolio.js
@@ -8,7 +8,7 @@ function Portfolio () {
             <div className="bg-paint-img w-screen h-screen bg-cover">
                 <div className="bg-indigo-900 w-screen h-screen bg-opacity-80">
                     <div className="p-6 flex flex-col flex-wrap">
-                        <h1 className="font-montserrat text-6xl font-bold text-pink-200 uppercase text-center p-6">Portfolio</h1>
+                        <h1 className="font-montserrat text-3xl font-bold text-pink-200 uppercase text-center p-6">Portfolio</h1>
                     </div>
                     <div className="flex flex-row flex-wrap justify-center align-center text-center">
                         <div className="bg-pink-300 max-w-3xl rounded-md shadow-xl p-6 m-6">
